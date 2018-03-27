0678dcf72ccf676521ed2b0a9a9dfe403a499062
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-03-27 09:36:40 +0200

    add bootstrap style

--- application.py
+++ application.py
@@ -19,17 +19,22 @@ def hello_world():
 
 
 @app.route('/countries/<int:country_id>/JSON')
-def countries_json(country_id):
+def country_json(country_id):
     country = session.query(Country).filter_by(id=country_id).one()
     return jsonify(Country=country.serialize)
 
 
 @app.route('/countries/<int:country_id>/cities/JSON')
 def country_cities_json(country_id):
-    country = session.query(Country).filter_by(id=country_id).one()
     cities = session.query(CatalogItem).filter_by(
         country_id=country_id).all()
     return jsonify(Cities=[i.serialize for i in cities])
+
+
+@app.route('/countries/JSON')
+def countries_json():
+    countries = session.query(Country).all()
+    return jsonify(Countries=[i.serialize for i in countries])
 
 
 # Show all categories
